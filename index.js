f1f6ec93f324172c1a7cbc3d6e8b5810d992707b
Hoang Phong <uzuno95@gmail.com>
2021-03-31 23:42:16 +0700

    config

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var expressHbs = require("express-handlebars");
+var app = express();
+app.listen(process.env.PORT || "3001");
+app.engine(
+  "handlebars",
+  expressHbs({
+    layoutsDir: __dirname + "/views/layouts",
+    // layout cha mặc định
+    defaultLayout: "main",
+  })
+);
+app.set("view engine", "handlebars");
+
+app.get("/home", function (request, response) {
+  response.render("home");
+});
+
+app.get("/profile", function (request, response) {
+  response.render("profile");
+});
+
+app.get("/update", function (request, response) {
+  response.render("update");
+});
